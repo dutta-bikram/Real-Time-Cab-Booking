cd868962d900aebb309835d02920c139faf96d24
bikram dutta <duttabikram872@gmail.com>
2026-04-07 09:46:00 +0530

    improved main for deployment error

--- app.py
+++ app.py
@@ -318,4 +318,4 @@ def get_pending_rides():
 # RUN SERVER
 # -------------------------------
 if __name__ == "__main__":
-    socketio.run(app, host="0.0.0.0", port=5000, debug=True)
+    socketio.run(app, host="0.0.0.0", port=5000, debug=True, allow_unsafe_werkzeug=True)
